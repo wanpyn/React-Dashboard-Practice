8f382777065a849bb11230f3c49adf5f8d8af825
Wanpynskhem Pyngrope <wanzpunkrock007@gmail.com>
2024-07-04 14:45:22 +0530

    added avatar icon code and file of it

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,6 +2,11 @@ import { CgMenuLeft } from "react-icons/cg";
 import { FaUserAlt } from "react-icons/fa";
 import DropDownMenu from "./DropDownMenu";
 import { useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { BsBoxFill } from "react-icons/bs";
+import { FaChartPie } from "react-icons/fa";
+import { PiListBulletsFill } from "react-icons/pi";
+import { FaBorderAll } from "react-icons/fa6";
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +17,7 @@ function Nav() {
     <>
       <nav className="flex font-roboto ">
         <CgMenuLeft
-          className={`bg-dark-green text-white text-4xl w-16 items-center pt-1 cursor-pointer 
+          className={`bg-dark-green text-white text-4xl w-14 items-center pt-1 cursor-pointer 
           }`}
           onClick={() => ToggleMenuHandle()}
         />
@@ -25,16 +30,34 @@ function Nav() {
       <section>
         <div
           className={`flex font-roboto bg-dark-green h-screen pt-5 w-40 ${
-            isOpen ? "w-40" : "w-16"
-          } duration-500`}
+            isOpen ? "w-40" : "w-14"
+          } duration-500 p-4 text-lg `}
         >
-          <ul className="text-white pl-9">
-            <li>Welcome Alex John</li>
-            <li>Dashboard</li>
+          <ul className=" text-white cursor-pointer">
+            <li className="flex">
+              <Avatar className="w-8">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <span className="text-xs"> Welcome</span> <span>Ally Krex</span>
+            </li>
+            <li>
+              <BsBoxFill />
+              Dashboard
+            </li>
             <li>Forms</li>
-            <li>UI Elements</li>
-            <li>Charts</li>
-            <li>Tables</li>
+            <li>
+              <PiListBulletsFill />
+              UI Elements
+            </li>
+            <li>
+              <FaChartPie />
+              Charts
+            </li>
+            <li>
+              <FaBorderAll />
+              Tables
+            </li>
           </ul>
         </div>
       </section>
